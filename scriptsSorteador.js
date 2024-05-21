a115d9691d537c91adecb5e43d2ad0c19050bfb1
jonathanjj9 <jonathan_silva9@outlook.com>
2024-05-21 12:34:53 -0300

    atualização de funcões

--- scriptsSorteador.js
+++ scriptsSorteador.js
@@ -3,8 +3,18 @@ function generateNumber(){
     const entre = Math.ceil (document.querySelector(".input-entre").value)
     const e = Math.floor (document.querySelector(".input-e").value)
 
-    const result = Math.floor (Math.random() * (e - entre + 1)) + entre;
+
+    if(e > entre & e >= 0 & entre >= 0){
+        const result = Math.floor (Math.random() * (e - entre + 1)) + entre;
+
+        alert(result)
+
+    }else{
+        
+        alert("O valor a esquerda precisa ser MENOR do que o valor a direita")
+    }
     
-    alert(result)
+    
+    
 
 }
